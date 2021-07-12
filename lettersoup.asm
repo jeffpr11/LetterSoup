f3aa1565b893101f91d6547ec9437580b1e80b0f
Erwing <erwingdelfines@hotmail.com>
2021-07-11 20:41:48 -0500

    Juego Avance

--- lettersoup.asm
+++ lettersoup.asm
@@ -14,12 +14,22 @@ opVariante db  "Existen 2 tipos de variante disponibles",13,10
            
 selTematica db ?
 selVariante db ?
+onlyLetras db "Escriba solo letras$"
+
 EscribaString db "Escriba la palabra encontrada o EXIT para rendierse: $"
 
 
              
-          
+ streq db 'Felicidades encontraste la palabra: ','$'
+  struneq db 'Strings are Unequal','$'          
 epa db "$"
+
+
+resp1_1 db "LEON$"
+resp1_2 db "MONO$"
+resp1_3 db "FOCA$"
+resp1_4 db "GATO$"
+resp1_5 db "BUHO$"
 
 
 Matriz1A   db  "P B C D E F G",13,10 
@@ -36,7 +46,15 @@ Matriz1B   db  "P B C D E F G",13,10
            db  "R B C D E F G",13,10 
            db  "O B C D E F G",13,10 
            db  "A B C D E F G",13,10 
-           db  "1 B C D E 1 B $"
+           db  "1 B C D E 1 B $" 
+
+
+resp2_1 db "BICI$"
+resp2_2 db "MOTO$"
+resp2_3 db "AUTO$"
+resp2_4 db "TREN$"
+resp2_5 db "BOTE$"
+
 
 Matriz2A   db  "P B C D E F G",13,10 
            db  "E B C D E F G",13,10 
@@ -52,7 +70,23 @@ Matriz2B   db  "P B C D E F G",13,10
            db  "R B C D E F G",13,10 
            db  "O B C D E F G",13,10 
            db  "A B C D E F G",13,10 
-           db  "A B C D E 2 B $"
+           db  "A B C D E 2 B $"   
+           
+
+resp3_1 db "JAVA$"
+resp3_2 db "RUBY$"
+resp3_3 db "PERL$"
+resp3_4 db "RUST$"
+resp3_5 db "LAVA$"
+
+validacionSensitive db 0
+
+puntajeResp1 db 1
+puntajeResp2 db 1
+puntajeResp3 db 1
+puntajeResp4 db 1
+puntajeResp5 db 1
+puntajeJuego db 0
            
 Matriz3A   db  "P B C D E F G",13,10 
            db  "E B C D E F G",13,10 
@@ -69,7 +103,10 @@ Matriz3B   db  "P B C D E F G",13,10
            db  "O B C D E F G",13,10 
            db  "A B C D E F G",13,10 
            db  "A B C D E 3 B $"
-                  
+
+StringSensitive DB 20 dup(?)
+resp1 db 'Petern$'
+resp2 db 'Petern$'                  
 StringEscrito DB 20  dup(?)
 
 
@@ -84,16 +121,36 @@ mov ah, 09h
 lea dx, bienvenida
 int 21h
 
+
+;inicio
+
+
+
+;fin
+  
+
 call menu1
 call menu2
 call imprimirMatriz
 
-call inputString
+cmp selTematica, 1
+je jugar1
+cmp selTematica, 2
+je jugar2
+jmp jugar3
 
-  mov ah, 09
-  lea dx, StringEscrito
-  int 21h 
-  call lineaNueva
+
+jugar1:
+call juego1
+
+jugar2:
+
+jugar3:
+
+
+call inputString
+call caseSensitive
+call compareStrings
 
 
 
@@ -278,6 +335,35 @@ imprimirMatriz3B proc near
 imprimirMatriz3B endp
 
 
+;JUEGO 1    
+juego1 proc near
+    
+    pedir:
+    call inputString
+    call caseSensitive
+    
+    cmp validacionSensitive, 1
+    je pedir
+    
+    call compareStrings1_1
+    call compareStrings1_2
+    call compareStrings1_3
+    call compareStrings1_4
+    call compareStrings1_5
+    
+    
+    
+
+    
+    
+    ret
+juego1 endp
+
+
+
+;FIN JUEGO 1
+
+
 ;INPUT STRING
 
 inputString proc near
@@ -333,8 +419,91 @@ call lineaNueva
        
     ret
 inputString endp 
+ ;FIN INPUT STRING
  
- 
+
+; << COMPARAR STRINGS >>
+;COMPARE STRINGS
+compareStrings1_1 proc near
+   mov ax, data
+    mov ds, ax
+    mov es, ax
+    mov cx, 4
+    lea si, StringSensitive
+    lea di, resp1_1
+    rep cmpsb
+    jnz Not_Equal
+    lea dx, streq
+    jmp Equal 
+
+    Not_Equal:
+    jmp finComparar
+
+    Equal:
+    call lineaNueva 
+    mov ah, 09h
+    int 21h 
+    lea dx, resp1_1  
+    int 21h
+    mov al, puntajeResp1
+    add puntajeJuego, al
+    mov puntajeResp1, 0 
+    call lineaNueva
+    finComparar:
+     
+   ret
+compareStrings1_1 endp 
+;FIN Compare STRINGS
+;<< FIN COMPARAR STRINGS >>
+
+;Case sentitive
+caseSensitive proc near
+    mov ax, 0h
+    mov bx, offset StringEscrito
+    mov cx, 0h
+    mov cx, 4
+    mov validacionSensitive, 0
+    mov si, offset StringSensitive
+
+    validar:
+    cmp [bx], 41h
+    jae talVezLetra 
+
+    ingreseLetras:
+    lea dx, onlyLetras
+    mov ah, 09h
+    int 21h
+    mov validacionSensitive, 1
+    jmp pedirdenuevo
+
+    talVezLetra:
+    cmp [bx], 5Ah 
+    jbe esMayuscula
+    cmp [bx], 61h
+    jae esMinuscula
+
+    esMayuscula:
+    jmp transformarLetra
+
+    esMinuscula:
+    cmp [bx], 7Ah
+    jae ingreseLetras
+    sub [bx], 32
+    jbe transformarLetra 
+
+    transformarLetra:
+    mov dx, [bx]
+    mov [si], dx
+    inc bx 
+    inc si
+    loop validar
+    
+    pedirdenuevo:
+    ret
+caseSensitive endp    
+
+
+;FIN CASE sensitive
  
 
 
